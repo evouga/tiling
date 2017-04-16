8d469cb44c111ba7ffe1c4a9e51492bc5b32357c
Nathan Clement <nclement@cs.utexas.edu>
2017-04-16 12:55:34 -0500

    Removed libtourtre; also fixed an error that prevented vertices with the same x- or y-value from being added to the contour

--- SliceParser.cpp
+++ SliceParser.cpp
@@ -11,94 +11,96 @@ using namespace tinyxml2;
 
 void removeDuplicateVerts(Contour &c)
 {
-	vector<double> newx;
-	vector<double> newy;
-	int numpts = c.x.size();
-	for(int i=0; i<numpts; i++)
-	{
-		int prev = (i==0 ? numpts-1 : i-1);
-		if(c.x[prev] != c.x[i] && c.y[prev] != c.y[i])
-		{
-			newx.push_back(c.x[i]);
-			newy.push_back(c.y[i]);
-		}
-	}
-	c.x = newx;
-	c.y = newy;
+  vector<double> newx;
+  vector<double> newy;
+  int numpts = c.x.size();
+  for(int i=0; i<numpts; i++)
+  {
+    int prev = (i==0 ? numpts-1 : i-1);
+    if(c.x[prev] != c.x[i] || c.y[prev] != c.y[i])
+    {
+      newx.push_back(c.x[i]);
+      newy.push_back(c.y[i]);
+    }
+  }
+  c.x = newx;
+  c.y = newy;
 }
 
 class ContourVisitor : public XMLVisitor
 {
-	public:
-	ContourVisitor(const char *cname) : contourname(cname) {}
+ public:
+  ContourVisitor(const char *cname) : contourname(cname) {}
 
-	virtual bool VisitEnter (const XMLElement &e, const XMLAttribute *a)
-	{
-		if(strcmp(e.Name(), "Contour"))
-			return true;
-		const char *cname = e.Attribute("name");
-		if(!strcmp(cname, contourname))
-		{
-			const char *val = e.Attribute("points");
-			stringstream vals(val);
-			Contour contour;
-			while(vals)
-			{
-				double x,y;
-				char dummy;
-				vals >> x;
-				vals >> y;
-				vals >> dummy;
-				contour.x.push_back(x);
-				contour.y.push_back(y);
-			}
-			removeDuplicateVerts(contour);
-			contours.push_back(contour);
-		}
-		return true;
-	}
+  virtual bool VisitEnter (const XMLElement &e, const XMLAttribute *a)
+  {
+    if(strcmp(e.Name(), "Contour")) {
+      return true;
+    }
+    const char *cname = e.Attribute("name");
+    if(!strcmp(cname, contourname))
+    {
+      const char *val = e.Attribute("points");
+      stringstream vals(val);
+      Contour contour;
+      while(vals)
+      {
+        double x,y;
+        char dummy;
+        vals >> x;
+        vals >> y;
+        vals >> dummy;
+        contour.x.push_back(x);
+        contour.y.push_back(y);
+      }
+      removeDuplicateVerts(contour);
+      contours.push_back(contour);
+    }
+    return true;
+  }
 
-	vector<Contour> contours;
+  vector<Contour> contours;
 
-	private:
-	const char *contourname;
+ private:
+  const char *contourname;
 };
 
 Slice *readSlice(const char *filename, const char *objectname)
 {
-	ContourVisitor cv(objectname);
-	XMLDocument doc;
+  ContourVisitor cv(objectname);
+  XMLDocument doc;
 
-	if(doc.LoadFile(filename))
-		return NULL;
+  if(doc.LoadFile(filename))
+    return NULL;
 
-	doc.Accept(&cv);
+  doc.Accept(&cv);
 
   double thickness;
 
-	if(doc.FirstChildElement("Section")->QueryDoubleAttribute("thickness", &thickness) != XML_NO_ERROR) {
+  if(doc.FirstChildElement("Section")->QueryDoubleAttribute("thickness", &thickness) != XML_NO_ERROR) {
     printf("Returning NULL from %s:%d\n", __FILE__, __LINE__);
-		return NULL;
-	}
+    return NULL;
+  }
 
-	return new Slice(cv.contours, thickness);
+  return new Slice(cv.contours, thickness);
 }
 
 void readSlicesFromFolder(const char *baseFilename, const char *objectname, vector<Slice *> &slices) {
-	slices.clear();
-	int curslice = 0;
-	while (true) {
-		stringstream ss;
-		ss << baseFilename;
-		ss << ".";
-		ss << curslice;
-		Slice *slice = NULL;
-		if ((slice = readSlice(ss.str().c_str(), objectname)))
-			slices.push_back(slice);
-		else
+  slices.clear();
+  int curslice = 0;
+  while (true) {
+    stringstream ss;
+    ss << baseFilename;
+    ss << ".";
+    ss << curslice;
+    Slice *slice = NULL;
+    if ((slice = readSlice(ss.str().c_str(), objectname))) {
+      slices.push_back(slice);
+    } else {
       break;
-		curslice++;
-	}
+    }
+    curslice++;
+  }
   // Give all contours unique ids.
   int contour_id = 10;
   for (Slice *slice : slices)
